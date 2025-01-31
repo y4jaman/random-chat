eea59ff54a730e7930e7446064f901b76e6350a6
Gautam Yajaman <g.yajaman@gmail.com>
2025-01-31 22:24:41 +0000

    updated dist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); // Add this line
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env) => {
+  // Load environment variables from .env file (optional)
+  const envKeys = Object.keys(process.env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new webpack.DefinePlugin(envKeys), // Inject environment variables
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    port: 3000
-  },
+    devServer: {
+      port: 3000,
+    },
+  };
 };
